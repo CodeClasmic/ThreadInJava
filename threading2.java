8c4820caa848dae457a447978ab94733bc64a551
Aditya Chatterjee <151843924+CodeClasmic@users.noreply.github.com>
2024-12-18 00:12:57 +0530

    Add files via upload

---
+++ threading2.java
@@ -0,0 +1,9 @@
+public class threading2 {
+    public static void main(String[] args) {
+        thread T1=new thread("A");
+        T1.start();
+        thread T2=new thread("Adi");
+        T2.start();
+    }
+}
+
